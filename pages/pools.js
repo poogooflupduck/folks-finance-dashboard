66974afa7c3e28ff3a85b8d41875ccb1138e9b76
Karl Lee <poogooflupduck@gmail.com>
2022-04-14 00:31:49 +0000

    pool loading state, value locked and optimization ratio

--- pages/pools.js
+++ pages/pools.js
@@ -1,14 +1,94 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import NavBar from "@/components/NavBar";
-import { Box, Grid, GridItem, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  Flex,
+  Skeleton,
+  Text,
+  Spacer,
+  CircularProgress,
+  CircularProgressLabel,
+} from "@chakra-ui/react";
 import SwrData from "@/components/SwrData";
+
 const fetcher = (url) => fetch(url).then((r) => r.json());
+let nf = new Intl.NumberFormat("en-US");
+
+const PoolCardGrid = () => {
+  const { data, isLoading, isError } = SwrData("/api/tvl", fetcher);
+  if (isLoading)
+    return (
+      <>
+        {Array(6)
+          .fill("")
+          .map((_, i) => (
+            <GridItem
+              key={i}
+              colSpan={{ base: 2, sm: 2, md: 2, lg: 1 }}
+              p={6}
+              borderWidth={1}
+              borderRadius="md"
+            >
+              <Skeleton height="20px" />
+            </GridItem>
+          ))}
+      </>
+    );
+  if (isError) return <Skeleton height="20px" />;
+  return (
+    <>
+      {data.pools.map((pool) => (
+        <GridItem
+          key={pool.symbol}
+          colSpan={{ base: 2, sm: 2, md: 2, lg: 1 }}
+          borderWidth={1}
+          borderRadius="md"
+        >
+          <Flex align="center" p={6}>
+            <Image
+              src={"/icons/" + pool.symbol.toLowerCase() + ".svg"}
+              width={30}
+              height={30}
+            />
+            <Text ml={2}>{pool.symbol}</Text>
+            <Spacer />
+            <Text ml={2} color="grey">
+              {nf.format(pool.totalLocked)}
+            </Text>
+          </Flex>
+          <Box p={6} align="center">
+            <CircularProgress
+              value={(pool.utilizationRatio * 100) / 1e14}
+              color="green.400"
+              size={"80%"}
+            >
+              <CircularProgressLabel fontSize={"lg"}>
+                {((pool.utilizationRatio * 100) / 1e14).toFixed(2) + "%"}
+                <br />
+                <Text fontSize="sm">Utilization</Text>
+              </CircularProgressLabel>
+            </CircularProgress>
+          </Box>
+          <Box borderTopWidth={1} w="full" px={6} py={3}>
+            <Flex align="center">
+              <Text>Value locked</Text>
+              <Spacer />
+              <Text ml={2} color="grey">
+                {nf.format(pool.totalLockedValue) + " USD"}
+              </Text>
+            </Flex>
+          </Box>
+        </GridItem>
+      ))}
+    </>
+  );
+};
 
 const Pools = () => {
-  const { data, isLoading, isError } = SwrData("/api/allPools", fetcher);
-  console.log(data);
-
   return (
     <div>
       <Head>
@@ -18,25 +98,7 @@ const Pools = () => {
       </Head>
       <NavBar />
       <Grid templateColumns={"repeat(5, 1fr)"} gap={2} p={2}>
-        {data &&
-          data.map((pool) => (
-            <GridItem
-              key={pool.symbol}
-              colSpan={{ base: 2, sm: 2, md: 2, lg: 1 }}
-              p={6}
-              borderWidth={1}
-              borderRadius="md"
-            >
-              <Flex align="center">
-                <Image
-                  src={"/icons/" + pool.symbol.toLowerCase() + ".svg"}
-                  width={30}
-                  height={30}
-                />
-                <Box ml={2}>{pool.symbol}</Box>
-              </Flex>
-            </GridItem>
-          ))}
+        <PoolCardGrid />
       </Grid>
     </div>
   );
